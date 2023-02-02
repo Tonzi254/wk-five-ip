45d4e5a513e9111d6aa531c54b464c09c8f5ceeb
Grace Gicheru <gracegicheru3@gmail.com>
2023-02-02 19:45:53 +0300

    Added the CRUD routes for the application

--- routes/routes.js
+++ routes/routes.js
@@ -2,3 +2,29 @@ const express = require('express');
 
 const router= express.Router();
 module.exports= router;
+
+//CRUD routes
+
+//Create
+
+router.post('/students', (req, res)=>{ 
+    res.send('POST');
+});
+
+//Read
+
+router.get('/students', (req, res)=>{ 
+    res.send('GET');
+});
+
+//Update
+
+router.patch('/students/:id', (req, res)=>{ 
+    res.send('UPDATE');
+});
+
+//Delete
+
+router.delete('/students/:id', (req, res)=>{ 
+    res.send('DELETE');
+});
