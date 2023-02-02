744db9c8f20d9abaed9284d45cdf195ebee03e84
Grace Gicheru <gracegicheru3@gmail.com>
2023-02-02 19:17:08 +0300

    Add routes to the application

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const routes= require('./routes/routes');
 
 
 // DB Variables
@@ -26,6 +27,7 @@ database.once('connected', () => {
 const app = express();
 
 app.use(express.json());
+app.use('/', routes);
 
 
 app.listen(port, () => {
